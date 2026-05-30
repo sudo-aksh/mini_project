fc2c811eaba27fdce105df8677de23e946cbb120
buggu <aakashwankhade87@gmail.com>
2026-05-30 17:14:12 +0530

    made function for creating databse

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 import sqlite3
 
-conn = sqlite3.connect("database.db")
+def set_database():
+    conn = sqlite3.connect("database.db")
 
-curr = conn.cursor()
+    curr = conn.cursor()
 
 
-curr.execute(f"""CREATE TABLE IF NOT EXISTS students
-(id INTEGER PRIMANRY KEY,
-name TEXT NOT NULL,
-class INTEGER NOT NULL CHECK(class BETWEEN 5 AND 10),
-email TEXT UNIQUE NOT NULL)""")
+    curr.execute(f"""CREATE TABLE IF NOT EXISTS students
+    (id INTEGER PRIMANRY KEY,
+    name TEXT NOT NULL,
+    class INTEGER NOT NULL CHECK(class BETWEEN 5 AND 10),
+    email TEXT UNIQUE NOT NULL)""")
 
-conn.commit()
-conn.close()
+    conn.commit()
+    conn.close()
