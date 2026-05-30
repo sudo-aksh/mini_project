d351df81384057894a6bac1aee360f3a56205c4a
buggu <aakashwankhade87@gmail.com>
2026-05-30 17:04:49 +0530

    added table to store students data with name, id,class and email

--- app.py
+++ app.py
@@ -3,3 +3,13 @@ import sqlite3
 conn = sqlite3.connect("database.db")
 
 curr = conn.cursor()
+
+
+curr.execute(f"""CREATE TABLE IF NOT EXISTS students
+(id INTEGER PRIMANRY KEY,
+name TEXT NOT NULL,
+class INTEGER NOT NULL CHECK(class BETWEEN 5 AND 10),
+email TEXT UNIQUE NOT NULL)""")
+
+conn.commit()
+conn.close()
